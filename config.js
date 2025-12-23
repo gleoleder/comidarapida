7960478dbc76cddfd1753d1ee719a5e0ab881719
gleoleder <gleo90000@gmail.com>
2025-12-23 15:44:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const CONFIG = {
     // Este ID se encuentra en la URL de tu Google Sheet:
     // https://docs.google.com/spreadsheets/d/[ESTE_ES_EL_ID]/edit
     // ══════════════════════════════════════════════════════════════════════════
-    GOOGLE_SHEET_ID: '1AmFocVwvywXz6LOwggkFscXjEhx_FZvZCVmb-1ihm5I',
+    GOOGLE_SHEET_ID: '1hcQV0UaLWCbImY6uSxtQGbyrNkPmNP_lldzZMx61Pj0',
     
     // ══════════════════════════════════════════════════════════════════════════
     // CREDENCIALES DE GOOGLE API
@@ -26,8 +26,8 @@ const CONFIG = {
     // CLIENT_ID: Identifica la aplicación ante Google
     // API_KEY: Clave para acceder a la API de Google Sheets
     // ══════════════════════════════════════════════════════════════════════════
-    CLIENT_ID: '488089624210-ns62tr4g9rqov3k2b85965c4p4fto028.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyDsIk-N9hDAzZN7vc9b2rUIhcA7D8ViOFk',
+    CLIENT_ID: '814005655098-8csk41qts3okv4b2fjnq7ls4qc2kq0vc.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyAOhGTjJXHhuUhqf1g2DPCla59xNzftb-Q',
     
     // ══════════════════════════════════════════════════════════════════════════
     // NOMBRES DE LAS 5 HOJAS DE LA BASE DE DATOS
